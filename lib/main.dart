b991e044b75a447af303cbe5ffdda12209b8f7d3
Manh Hung <>
2024-12-13 18:29:54 +0700

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(MagicBallPage());
+}
+
+class MagicBallPage extends StatefulWidget {
+  const MagicBallPage({super.key});
+
+  @override
+  State<MagicBallPage> createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue.shade100,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade100,
+          title: Text('Ask me anything'),
+        ),
       ),
     );
+  }
+}
